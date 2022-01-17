5fadd98d0fbc62272451b6acd4ed3c264a0f73c7
Naveen Pratap <naveenpratapsingh45@gmail.com>
2022-01-17 10:12:31 +0530

    Update views.py

--- base/views.py
+++ base/views.py
@@ -9,8 +9,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
-import requests
-
 
 from .models import Task
 
